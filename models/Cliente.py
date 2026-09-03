9e0093847d16e73591592848e2e45e6fa45c86c5
ArtMainardi <iensenmainardi@gmail.com>
2026-09-03 11:27:14 -0300

    feat: adiciona model 'Cliente'

---
+++ models/Cliente.py
@@ -0,0 +1,18 @@
+class Cliente:
+    def __init__(self, nome, email, telefone):
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+        self.telefone = telefone
+
+    def exibir(self):
+        return (f"Nome: {self.nome}  |  Email: {self.email}  |  Telefone: {self.telefone}")
+
+    def converter_tupla(self):
+        return (self.nome, self.email, self.email)
+
+    @staticmethod
+    def reverter_tupla(tupla):
+        cliente = Cliente(tupla[1], tupla[2], tupla[3])
+        cliente.id = tupla[0]
+        return cliente
